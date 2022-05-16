29acf3825286b040167067f94a4e718860a3a778
Ansu26 <dimar_suarez82182@elpoli.edu.co>
2022-05-16 09:11:04 -0500

    Aggregado Distribucion. Creación de los atributos del Agregado.

--- domain/src/main/java/co/com/servientrega/logistica/distribucion/Distribucion.java
+++ domain/src/main/java/co/com/servientrega/logistica/distribucion/Distribucion.java
@@ -1,9 +1,15 @@
 package co.com.servientrega.logistica.distribucion;
 
+import co.com.servientrega.logistica.guia.Guia;
+import co.com.servientrega.logistica.guia.GuiaId;
 import co.com.sofka.domain.generic.AggregateEvent;
 
-public class Distribucion extends AggregateEvent<DistribucionId> {
+import java.util.Map;
 
+public class Distribucion extends AggregateEvent<DistribucionId> {
+    protected EmpleadoId empleadoId;
+    protected VehiculoId vehiculoId;
+    protected Map<GuiaId, Guia> guias;
 
     public Distribucion(DistribucionId entityId) {
         super(entityId);
