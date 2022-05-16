e0db1a5747ba0c6c385dbca6d627d5fb26c46588
Ansu26 <dimar_suarez82182@elpoli.edu.co>
2022-05-16 09:03:58 -0500

    Aggregado Distribucion. Creación de la clase principal Distribucion.

--- domain/src/main/java/co/com/servientrega/logistica/distribucion/Distribucion.java
+++ domain/src/main/java/co/com/servientrega/logistica/distribucion/Distribucion.java
@@ -1,5 +1,11 @@
 package co.com.servientrega.logistica.distribucion;
 
-public class Distribucion extends AggregateEvent<DistribucionId>{
+import co.com.sofka.domain.generic.AggregateEvent;
 
+public class Distribucion extends AggregateEvent<DistribucionId> {
+
+
+    public Distribucion(DistribucionId entityId) {
+        super(entityId);
+    }
 }
