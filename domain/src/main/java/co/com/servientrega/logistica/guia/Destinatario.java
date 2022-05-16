4207679d4c5736e1e66313beba8b7b5dbc2f5f5a
AngelaEspinosa <titagena@gmail.com>
2022-05-16 09:19:42 -0500

    AGREGADO GUIA: se crean las entidades hijas del agregado DESTINATARIO Y REMITENTE.

--- domain/src/main/java/co/com/servientrega/logistica/guia/Destinatario.java
+++ domain/src/main/java/co/com/servientrega/logistica/guia/Destinatario.java
@@ -1,2 +1,10 @@
-package co.com.servientrega.logistica.guia;public class Destinatario {
+package co.com.servientrega.logistica.guia;
+
+import co.com.servientrega.logistica.guia.values.DestinarioId;
+import co.com.sofka.domain.generic.Entity;
+
+public class Destinatario extends Entity<DestinarioId> {
+    public Destinatario(DestinarioId entityId) {
+        super(entityId);
+    }
 }
--- domain/src/main/java/co/com/servientrega/logistica/guia/Remitente.java
+++ domain/src/main/java/co/com/servientrega/logistica/guia/Remitente.java
@@ -1,2 +1,11 @@
-package co.com.servientrega.logistica.guia;public class Remitente {
+package co.com.servientrega.logistica.guia;
+
+import co.com.servientrega.logistica.guia.values.RemitenteId;
+import co.com.sofka.domain.generic.Entity;
+
+public class Remitente extends Entity<RemitenteId> {
+
+    public Remitente(RemitenteId entityId) {
+        super(entityId);
+    }
 }
